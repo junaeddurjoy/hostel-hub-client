59c43f95186f33106bb5b53ae869332f3ddc0330
junaeddurjoy <junaed.durjoy@gmail.com>
2023-11-23 18:46:24 +0600

    project routing setup

---
+++ src/Layout/Routes/Routes.jsx
@@ -0,0 +1,19 @@
+import {
+    createBrowserRouter,
+  } from "react-router-dom";
+import Root from "../Root";
+import App from "../../App";
+import Home from "../Pages/Home";
+
+  export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root></Root>,
+      children: [
+        {
+            path: "/",
+            element: <Home></Home>,
+        }
+      ]
+    },
+  ]);

4fe125b9ad11b294ea4667a4a6b4f5f14e7c8921
junaeddurjoy <junaed.durjoy@gmail.com>
2023-11-24 01:53:52 +0600

    banner carousel updated

--- src/Layout/Components/Banner.jsx
+++ src/Layout/Components/Banner.jsx
@@ -2,19 +2,15 @@ import Carousel from "./Carousel";
 
 const Banner = () => {
     return (
-        <div className="lg:mx-32 my-5">
-            <div className="hero bg-[#cce3de]">
-                <div className="hero-content flex-col lg:flex-row-reverse">
-                    {/* <img src="https://i.ibb.co/9qgh0Rg/friends.png" className="h-[250px] md:h-[350px] lg:h-[500px] w-[400px] md:w-[700px] rounded-lg" /> */}
-                    {/* <img src="https://cdni.iconscout.com/illustration/premium/thumb/friends-enjoying-dinner-3736058-3123360.png" className=" rounded-lg" /> */}
-                    <div>
-                        <Carousel></Carousel>
-                    </div>
-                    <div className="lg:ml-20">
-                        <div className="badge badge-accent text-white bg-[#34a0a4] border-[#34a0a4] p-3">Welcome to</div>
-                        <h1 className="md:text-3xl lg:text-5xl lg:pb-5 text-center font-bold">HostelHub!</h1>
-                        <h1 className="text-md text-center ">Multi cuisine cafe | Complimentary food items | Two course meals | Premium food with affordable price </h1>
-                    </div>
+        <div className="lg:mx-32 my-5 bg-[#cce3de]">
+            <div className="lg:flex items-center justify-center p-5">
+                <div className="lg:ml-20 my-5 md:my-8 lg:my-0">
+                    <h1 className="badge badge-accent ml-36 md:ml-[20rem] lg:ml-0 text-white mx-auto bg-[#34a0a4] border-[#34a0a4] p-3">Welcome to</h1>
+                    <h1 className="text-2xl md:text-5xl lg:text-5xl lg:pb-5 text-center font-bold">HostelHub!</h1>
+                    <h1 className="text-md text-center ">Multi cuisine cafe | Complimentary food items | Two course meals | Premium food with affordable price </h1>
+                </div>
+                <div>
+                    <Carousel></Carousel>
                 </div>
             </div>
         </div>
--- src/Layout/Components/Carousel.jsx
+++ src/Layout/Components/Carousel.jsx
@@ -1,3 +1,4 @@
+import { FaAngleLeft,FaAngleRight } from "react-icons/fa6";
 const Carousel = () => {
     return (
         <div>
@@ -5,29 +6,29 @@ const Carousel = () => {
                 <div id="slide1" className="carousel-item relative w-full">
                     <img src="https://cdni.iconscout.com/illustration/premium/thumb/friends-playing-table-games-3736059-3123361.png?f=webp" className="mx-auto" />
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide4" className="btn btn-circle"></a>
-                        <a href="#slide2" className="btn btn-circle"></a>
+                        <a href="#slide4" className="btn btn-circle text-black"><FaAngleLeft /></a>
+                        <a href="#slide2" className="btn btn-circle text-black"><FaAngleRight /></a>
                     </div>
                 </div>
                 <div id="slide2" className="carousel-item relative w-full">
                     <img src="https://cdni.iconscout.com/illustration/premium/thumb/friends-enjoying-dinner-3736058-3123360.png" className="mx-auto" />
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide1" className="btn btn-circle"></a>
-                        <a href="#slide3" className="btn btn-circle"></a>
+                        <a href="#slide1" className="btn btn-circle text-black"><FaAngleLeft /></a>
+                        <a href="#slide3" className="btn btn-circle text-black"><FaAngleRight /></a>
                     </div>
                 </div>
                 <div id="slide3" className="carousel-item relative w-full">
                     <img src="https://cdni.iconscout.com/illustration/premium/thumb/couple-on-romantic-dinner-date-at-home-3736061-3123363.png?f=webp" className="mx-auto" />
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide2" className="btn btn-circle"></a>
-                        <a href="#slide4" className="btn btn-circle"></a>
+                        <a href="#slide2" className="btn btn-circle text-black"><FaAngleLeft /></a>
+                        <a href="#slide4" className="btn btn-circle text-black"><FaAngleRight /></a>
                     </div>
                 </div>
                 <div id="slide4" className="carousel-item relative w-full">
                     <img src="https://i.ibb.co/9qgh0Rg/friends.png" className="mx-auto" />
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide3" className="btn btn-circle"></a>
-                        <a href="#slide1" className="btn btn-circle"></a>
+                        <a href="#slide3" className="btn btn-circle text-black"><FaAngleLeft /></a>
+                        <a href="#slide1" className="btn btn-circle text-black"><FaAngleRight /></a>
                     </div>
                 </div>
             </div>

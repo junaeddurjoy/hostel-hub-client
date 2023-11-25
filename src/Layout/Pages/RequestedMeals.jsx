b5491468c95338997afa47520d296995bd59c0e3
junaeddurjoy <junaed.durjoy@gmail.com>
2023-11-25 17:05:39 +0600

    requested meals

--- src/Layout/Pages/RequestedMeals.jsx
+++ src/Layout/Pages/RequestedMeals.jsx
@@ -2,10 +2,11 @@ import { Link } from "react-router-dom";
 import { CgProfile } from "react-icons/cg";
 import { IoPersonAddOutline } from "react-icons/io5";
 import { MdOutlineRateReview } from "react-icons/md";
+import { MdCancel } from "react-icons/md";
 const RequestedMeals = () => {
     return (
         <div className="w-full flex">
-           <div className="w-1/6 bg-gray-200">
+            <div className="w-1/6 bg-gray-200">
                 <ul className="menu menu-lg bg-gray-200 w-56 rounded-box">
                     <Link to={'/userdashboard'}><li className="font-semibold"><a><CgProfile className="text-3xl text-green-600" />My Profile</a></li></Link>
                     <Link to={'/requestedmeals'}><li className="font-semibold"><a><IoPersonAddOutline className="text-3xl text-green-600" /> Requested Meals</a></li></Link>
@@ -13,7 +14,43 @@ const RequestedMeals = () => {
                 </ul>
             </div>
             <div className="w-5/6">
-                req
+                <div className="overflow-x-auto">
+                    <table className="table">
+                        {/* head */}
+                        <thead>
+                            <tr>
+                                <th className="text-xl font-bold">Item</th>
+                                <th className="text-xl font-bold ">Ingredients</th>
+                                <th className="text-xl font-bold">Type</th>
+                                <th className="text-xl font-bold">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {/* row 1 */}
+                            <tr>
+                                <td>
+                                    <div className="flex items-center gap-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-20 h-20">
+                                                <img src="https://www.kannammacooks.com/wp-content/uploads/chettinadu-chicken-biriyani-1-3.jpg" alt="Avatar Tailwind CSS Component" />
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <div className="font-bold text-xl">Biriyani</div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td className="text-lg">
+                                    Meat,Rice
+                                </td>
+                                <td className="text-lg">Lunch</td>
+                                <th>
+                                    <button className="btn btn-ghost btn-md"><MdCancel className="text-3xl text-red-600" /></button>
+                                </th>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     );
